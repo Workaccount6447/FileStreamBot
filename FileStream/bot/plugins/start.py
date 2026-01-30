a4be123cc169dcc3c0b8c6481055d27a0df01003
Workaccount6447 <sndj86779@gmail.com>
2026-01-30 16:00:15 +0530

    Update start.py

--- FileStream/bot/plugins/start.py
+++ FileStream/bot/plugins/start.py
@@ -86,19 +86,13 @@ async def start(bot: Client, message: Message):
 async def start(bot, message):
     if not await verify_user(bot, message):
         return
-    if Telegram.START_PIC:
-        await message.reply_photo(
-            photo=Telegram.START_PIC,
-            caption=LANG.ABOUT_TEXT.format(FileStream.fname, __version__),
-            parse_mode=ParseMode.HTML,
-            reply_markup=BUTTON.ABOUT_BUTTONS
-        )
-    else:
-        await message.reply_text(
-            text=LANG.ABOUT_TEXT.format(FileStream.fname, __version__),
-            disable_web_page_preview=True,
-            reply_markup=BUTTON.ABOUT_BUTTONS
-        )
+
+    await message.reply_text(
+        text=LANG.ABOUT_TEXT.format(FileStream.fname, __version__),
+        parse_mode=ParseMode.HTML,
+        disable_web_page_preview=True,
+        reply_markup=BUTTON.ABOUT_BUTTONS
+    )
 
 @FileStream.on_message((filters.command('help')) & filters.private)
 async def help_handler(bot, message):
@@ -125,26 +119,36 @@ async def help_handler(bot, message):
 async def my_files(bot: Client, message: Message):
     if not await verify_user(bot, message):
         return
+
     user_files, total_files = await db.find_files(message.from_user.id, [1, 10])
 
     file_list = []
     async for x in user_files:
-        file_list.append([InlineKeyboardButton(x["file_name"], callback_data=f"myfile_{x['_id']}_{1}")])
+        file_list.append([
+            InlineKeyboardButton(
+                x["file_name"],
+                callback_data=f"myfile_{x['_id']}_{1}"
+            )
+        ])
+
     if total_files > 10:
-        file_list.append(
-            [
-                InlineKeyboardButton("◄", callback_data="N/A"),
-                InlineKeyboardButton(f"1/{math.ceil(total_files / 10)}", callback_data="N/A"),
-                InlineKeyboardButton("►", callback_data="userfiles_2")
-            ],
-        )
+        file_list.append([
+            InlineKeyboardButton("◄", callback_data="N/A"),
+            InlineKeyboardButton(f"1/{math.ceil(total_files / 10)}", callback_data="N/A"),
+            InlineKeyboardButton("►", callback_data="userfiles_2")
+        ])
+
     if not file_list:
-        file_list.append(
-            [InlineKeyboardButton("ᴇᴍᴘᴛʏ", callback_data="N/A")],
-        )
-    file_list.append([InlineKeyboardButton("ᴄʟᴏsᴇ", callback_data="close")])
-    await message.reply_photo(photo=Telegram.FILE_PIC,
-                              caption="Total files: {}".format(total_files),
-                              reply_markup=InlineKeyboardMarkup(file_list))
+        file_list.append([
+            InlineKeyboardButton("ᴇᴍᴘᴛʏ", callback_data="N/A")
+        ])
 
+    file_list.append([
+        InlineKeyboardButton("ᴄʟᴏsᴇ", callback_data="close")
+    ])
 
+    await message.reply_text(
+        text=f"Total files: {total_files}",
+        reply_markup=InlineKeyboardMarkup(file_list),
+        disable_web_page_preview=True
+    )
