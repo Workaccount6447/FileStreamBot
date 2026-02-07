23a6c475971df6339e242b0901e35b44eb374398
Workaccount6447 <sndj86779@gmail.com>
2026-02-07 18:41:14 +0530

    Update translation.py

--- FileStream/utils/translation.py
+++ FileStream/utils/translation.py
@@ -41,7 +41,8 @@ Thanks
 <i>Click ⭐ Donate to proceed</i>
 """
 
-    STREAM_TEXT = """
+    
+STREAM_TEXT = """
 <i><u>𝗬𝗼𝘂𝗿 𝗟𝗶𝗻𝗸 𝗚𝗲𝗻𝗲𝗿𝗮𝘁𝗲𝗱 !</u></i>\n
 <b>📂 Fɪʟᴇ ɴᴀᴍᴇ :</b> <b>{}</b>\n
 <b>📦 Fɪʟᴇ ꜱɪᴢᴇ :</b> <code>{}</code>\n
