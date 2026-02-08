66651ba7b506c1061c770e0db34311139f34203c
Workaccount6447 <sndj86779@gmail.com>
2026-02-08 18:38:41 +0530

    Update donate.py

--- FileStream/utils/donate.py
+++ FileStream/utils/donate.py
@@ -19,11 +19,11 @@ async def donate_menu(_, query):
     data = query.data.split("_")
     current_amount = int(data[1]) if len(data) > 1 else 10
     
-    text = "<b>Why should you donate to Royality Bots?\n\n
+    text = f"""<b>Why should you donate to Royality Bots?\n\n
 
 • It helps to cover the cost of the servers.\n
 • It motivate us to make an update or create a new bot.\n
-• Help me to buy a cup of tea from starbucks (does starbucks provides tea ?)<\b>\n-----------------------------------\n👇 <b>Choose an amount to donate:</b>"
+• Help me to buy a cup of tea from starbucks (does starbucks provides tea ?)<\b>\n-----------------------------------\n👇 <b>Choose an amount to donate:</b>"""
     
     buttons = [
         [
